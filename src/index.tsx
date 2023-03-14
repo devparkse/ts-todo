560636a0619821b98e9d4c780b58d396de7d8943
Park Sieun <pmj8856@naver.com>
2023-03-14 12:01:32 +0900

    redux 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppContainer from "./AppContainer";
 
+// redux 적용하기
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+// persist 적용
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+export let persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<AppContainer />);
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <AppContainer />
+    </PersistGate>
+  </Provider>
+);
